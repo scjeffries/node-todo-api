72797042d12c26d2a9a018359949478f31a6c651
scjeffries <scott@teamgauges.com>
2017-08-28 20:49:30 -0400

    Add tests for GET /users/me and POST /users.

--- server/models/user.js
+++ server/models/user.js
@@ -59,7 +59,7 @@ UserSchema.statics.findByToken = function(token) {
     let decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123')
+        decoded = jwt.verify(token, 'abc123');
     } catch(e) {
         // return new Promise((resolve, reject) => {
         //     reject();
